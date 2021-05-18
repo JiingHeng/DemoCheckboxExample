ba2d1716257c41c5fa041607160ba44665c1b0e8
20013327 <20013327@myrp.edu.sg>
2021-05-18 14:01:19 +0800

    finish

--- app/src/main/java/sg/edu/rp/c346/id20013327/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20013327/democheckboxexample/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20013327.democheckboxexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -27,18 +28,21 @@ public class MainActivity extends AppCompatActivity {
         btnCheck.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                Log.i("MyActivity", "Inside onClick()");
                 Toast.makeText(MainActivity.this,"Button Click", Toast.LENGTH_LONG).show();
 
-                if(cbEnabled.isChecked() == true) {
-                    tvShow.setText("The discount is given");
+                if(cbEnabled.isChecked()) {
+                    double pay = calcPay(100,20);
+                    tvShow.setText("The discount is given. You need to pay " + pay);
                 } else {
-                    tvShow.setText("The discount is not given");
+                    double pay = calcPay(100,0);
+                    tvShow.setText("The discount is not given. You need to pay " + pay);
                 }
             }
         });
-
-
-
-
+    }
+    private double calcPay(double price, double discount) {
+        double pay = price * (1 - discount/100 );
+        return pay;
     }
 }
